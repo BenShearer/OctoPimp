87bf6ef3358a3550289fdd30523fa0fb4721aaa4
BenShearer <ben.shearer@me.com>
2015-03-16 16:44:32 +0000

    Stareted stuff

---
+++ OctoPimp/OctoPimp.Web/Controllers/VariableSetController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Octopus.Client;
+using Octopus.Client.Model;
+
+namespace OctoPimp.Web.Controllers
+{
+    public class VariableSetController : Controller {
+
+        private OctopusClient Client;
+        private OctopusRepository Repo;
+        private string Endpoint;
+        private string ApiKey;
+        public VariableSetController() {
+
+            Endpoint = ConfigurationManager.AppSettings["Octopus.Api.Endpoint"];
+            ApiKey = ConfigurationManager.AppSettings["Octopus.Api.Key"];
+
+            Client = new OctopusClient(new OctopusServerEndpoint(Endpoint,ApiKey));
+            Repo = new OctopusRepository(new OctopusServerEndpoint(Endpoint, ApiKey));
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        private 
+
+    }
+}
---
+++ OctoPimp/OctoPimp/OctoServiceWrapper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Octopus.Client;
+using Octopus.Client.Model;
+
+namespace OctoPimp {
+    public class OctoServiceWrapper {
+        private readonly IOctopusClient octopusClient;
+
+        public OctoServiceWrapper(IOctopusClient octopusClient) {
+            this.octopusClient = octopusClient;
+        }
+
+        public IEnumerable<OctoVariableSet_ViewModel> GetAll() {
+            //var vSets = octopusRepository.LibraryVariableSets.FindAll();
+
+            //foreach (var vs in vSets) {
+
+            //    yield return new OctoVariableSet_ViewModel {
+            //        Name = vs.Name,
+            //        Description = vs.Description,
+            //        Id = vs.Id
+            //    };
+            //}
+
+            var librarySets = octopusClient.List<VariableSetResource>();
+            librarySets.Items.ToList().ForEach(x => {
+                
+                
+            });
+        }
+    }
+
+    public class OctoVariableSet_ViewModel {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<> 
+    }
+}
